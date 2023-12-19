49a5baa2801a566e00f2016646473ddc44102ce3
dagimalemux <133212131+dagimalemux@users.noreply.github.com>
2023-12-19 12:12:56 +0000

    adding a game loop

--- script.js
+++ script.js
@@ -5,8 +5,11 @@ let ctx = canvas.getContext("2d")
 let width = canvas.width;
 let height = canvas.height;
 
+let xSpeed = 4;
+let ySpeed = 2;
 
 const BALL_SIZE = 6;
+let ballPosition = {x: 20, y:30}
 
 function draw(){
 
@@ -14,7 +17,7 @@ function draw(){
   ctx.fillStyle = "darkorange";
   ctx.fillRect(0,0,width, height)
   
-  let ballPosition = {x: 20, y:30}
+  
   ctx.fillStyle = "white"
   ctx.fillRect(ballPosition.x, ballPosition.y, BALL_SIZE, BALL_SIZE)
   
@@ -22,12 +25,35 @@ function draw(){
 
 draw();
 
-Almost all games contain game loop that orchestrates everything that has to happen for each frame of the game. Game loops are similar to animation loops. ut with some additional logic. here is the general shape of the game loop in most games:
+// Almost all games contain game loop that orchestrates everything that has to happen for each frame of the game. Game loops are similar to animation loops. ut with some additional logic. here is the general shape of the game loop in most games:
 
-Clear canvas
-Draw image
-get player input
-Update state
-Check collisions
-Wait a short time 
-Repeat 
+// Clear canvas
+// Draw image
+// get player input
+// Update state
+// Check collisions
+// Wait a short time 
+// Repeat 
+
+function update(){
+  ballPosition.x += xSpeed;
+  ballPosition.y += ySpeed;
+}
+
+function gameLoop(){
+  draw();
+  update();
+}
+gameLoop();
+
+
+
+
+
+
+
+
+
+
+
+ 
